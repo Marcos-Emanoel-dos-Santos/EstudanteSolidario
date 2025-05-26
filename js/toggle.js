361ef5bf31a90357415ebac1f66969e696c36de5
unknown <marcosssantos1997@gmail.com>
2025-05-26 14:44:15 -0300

    Tela de login completa com responsividade.

--- js/toggle.js
+++ js/toggle.js
@@ -4,12 +4,15 @@ export function toggleMenu(){
 }
 
 export function loginPopUp(){
-  const profile = document.getElementById('profile');
-  const login = document.getElementById('login');
   const loginPopUp = document.getElementById('div-login');
   const overlay = document.getElementById('overlay');
-  login.classList.add('hide');
-  profile.classList.add('show');
   loginPopUp.classList.add('show');
   overlay.classList.add('show');
+}
+
+export function exitLoginPopup(){
+  const loginPopUp = document.getElementById('div-login');
+  const overlay = document.getElementById('overlay');
+  loginPopUp.classList.remove('show');
+  overlay.classList.remove('show');
 }
