be646c0e15e4950f7897c53049e13d65dc2c1e15
unknown <marcosssantos1997@gmail.com>
2025-05-26 09:57:19 -0300

    inicio dos blocos para cada atividade.

--- js/toggle.js
+++ js/toggle.js
@@ -6,6 +6,10 @@ export function toggleMenu(){
 export function loginPopUp(){
   const profile = document.getElementById('profile');
   const login = document.getElementById('login');
-  login.classList.add('show');
+  const loginPopUp = document.getElementById('div-login');
+  const overlay = document.getElementById('overlay');
+  login.classList.add('hide');
   profile.classList.add('show');
+  loginPopUp.classList.add('show');
+  overlay.classList.add('show');
 }
